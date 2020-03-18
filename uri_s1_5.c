7ab00bdc264eb35a9bd2f24b681e9669484b5d55
root <joaov.aporto@gmail.com>
2020-03-18 14:11:46 +0000

    local repo sync

--- uri_s1_5.c
+++ uri_s1_5.c
@@ -1,58 +1,25 @@
 #include <stdio.h>
 
-void bucketsort(int m[], int m_size);
-
 int main() {
-  int N, Q;
-  int test_case = 0;
+
+  int n = 3;
+  int funcs[n];
+  int cost[n];
   
-  while (scanf("%d %d", &N, &Q)) {
-    if (N == 0 && Q == 0) break;
+  int i;
+  for (i = 0; i < n; i++) scanf("%d", &funcs[i]);
 
-    printf("CASE# %d:\n", ++test_case);
-    int marble[N];
-    int query[Q];
-    
-    int i;
-    for (i = 0; i < N; i++) scanf("%d", &marble[i]);
-    for (i = 0; i < Q; i++) scanf("%d", &query[i]);
+  cost[0] = funcs[1] * 2 + funcs[2] * 4;
+  cost[1] = funcs[0] * 2 + funcs[2] * 2;
+  cost[2] = funcs[0] * 4 + funcs[1] * 2;
 
-    bucketsort(marble, N);
-    
-    for (i = 0; i < Q; i++) {
-      int j;
-      for (j = 0; j < N; j++)
-	if (query[i] == marble[j]) {
-	  printf("%d found at %d\n", query[i], j+1);
-	  break;
-	}
+  int lower_cost = cost[0];
 
-      if (j == N) printf("%d not found\n", query[i]);
-    }
-  }
+  for (i = 1; i < n; i++)
+    if (lower_cost > cost[i])
+      lower_cost = cost[i];
+
+  printf("%d\n", lower_cost);  
 
   return 0;  
-}
-
-void bucketsort(int m[], int m_size) {
-  int max_n = 10000;
-  int buckets[max_n];
-
-  int i;
-  for (i = 0; i < max_n; i++)
-    buckets[i] = 0;
-
-  for (i = 0; i < m_size; i++)
-    buckets[m[i]-1] += 1;
-  
-  int j = 0;
-  for (i = 0; i < max_n; i++) {
-    if (buckets[i] == 0) continue;
-
-    int k;
-    for (k = j; k < j + buckets[i]; k++)
-      m[k] = i+1;
-
-    j += k-j;
-  }
 }
